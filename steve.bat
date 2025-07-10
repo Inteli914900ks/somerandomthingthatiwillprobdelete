dec214013abdbb3c403e17747fae30c1ca71e439
Inteli914900ks <intel.i9.14900ks@gmail.com>
2025-07-10 20:37:34 +0200

    Update and rename add steve.bat to steve.bat

--- steve.bat
+++ steve.bat
@@ -4,3 +4,5 @@ echo function koto:steve>> tick.mcfunction
 echo execute as @a if items entity @s container.* minecraft:skeleton_skull[minecraft:custom_name=Steve] run function koto:givesteve >> steve.mcfunction
 echo clear @s minecraft:skeleton_skull[minecraft:custom_name=Steve] 1>> givesteve.mcfunction
 echo give @s minecraft:player_head[custom_name=Steve] 1>> givesteve.mcfunction
+cd ..\..\..
+del steve.bat
